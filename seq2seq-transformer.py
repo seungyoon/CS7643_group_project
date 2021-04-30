f05ee9901c2feaa0d8b8801899c7bc1f17da3ad3
Yoon Lee <yoonl@Yoons-MacBook-Pro.local>
2021-04-30 14:48:48 -0500

    fix UniversalTransformer parameters
    Signed-off-by: Yoon Lee <yoonl@Yoons-MacBook-Pro.local>

--- seq2seq-transformer.py
+++ seq2seq-transformer.py
@@ -99,7 +99,7 @@ class TransformerModel(nn.Module):
         if model_type == "Vanilla":
             self.transformer = VanillaTransformer(d_model=emsize, nhead=nhead, num_encoder_layers=enc_layers, num_decoder_layers=dec_layers, dim_feedforward=hidden*4, dropout=dropout, activation='relu')
         elif model_type == "Universal":
-            self.transformer = UniversalTransformer(d_model=emsize, nhead=nhead, num_encoder_layers=enc_layers, num_decoder_layers=dec_layers, dim_feedforward=hidden*4, dropout=dropout, activation='relu')
+            self.transformer = UniversalTransformer(d_model=emsize, nhead=nhead, dropout=dropout, transition_type='fully_connected')
         self.fc_out = nn.Linear(emsize, outtoken)
 
         self.src_mask = None
